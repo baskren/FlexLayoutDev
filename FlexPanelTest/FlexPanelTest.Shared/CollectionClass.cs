72fca5f5f113cb781fe143aaf3d2bfb25ecded94
Ben Askren <ben@buildcalc.com>
2020-09-13 18:45:11 -0400

    Name change to FlexPanel;

---
+++ FlexPanelTest/FlexPanelTest.Shared/CollectionClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlexPanelTest
+{
+    public class Item : List<Item>, IList
+    {
+        /*
+		public IEnumerator<FlexItem> GetEnumerator()
+        {
+            return System.Linq.Enumerable.Empty<FlexItem>().GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return System.Linq.Enumerable.Empty<FlexItem>().GetEnumerator();
+        }
+
+	
+		public void Add(FlexItem child)
+		{
+		}
+      */
+
+	}
+}
---
+++ FlexPanelTest/FlexPanelTest.macOS/Main.cs
@@ -0,0 +1,15 @@
+﻿using AppKit;
+
+namespace FlexPanelTest.macOS
+{
+	static class MainClass
+	{
+		static void Main(string[] args)
+		{
+			NSApplication.Init();
+			NSApplication.SharedApplication.Delegate = new App();
+			NSApplication.Main(args);  
+		}
+	}
+}
+
